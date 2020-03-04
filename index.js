80af6ab4e50e23bb80e4f6b701aa28402daa4dea
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-03-04 13:42:39 -0500

    add index.js with the main package exports

---
+++ index.js
@@ -0,0 +1,9 @@
+const { DOMParser } = require('./lib/dom-parser')
+
+const { DOMImplementation, XMLSerializer } = require('./lib/dom')
+
+module.exports = {
+  DOMParser,
+  DOMImplementation,
+  XMLSerializer
+}
