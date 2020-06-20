a6cc7d9bbe1d299c43ad098ac67d37890f7b2dd7
Christian Bewernitz <coder@karfau.de>
2020-06-20 22:05:20 +0200

    test(xss): Convert xss-test to vows test suite

--- test/xss-test.js
+++ test/xss-test.js
@@ -1,5 +1,6 @@
-var XMLSerializer = require('../../lib/dom-parser').XMLSerializer
-var DOMParser = require('../../lib/dom-parser').DOMParser;
+var assert = require('assert');
+var vows = require('vows');
+var DOMParser = require('../lib/dom-parser').DOMParser;
 var domParser = new DOMParser({xmlns:{'':'http://www.w3.org/1999/xhtml'}});
 
 var excludeTags = new RegExp('^(?:'+['javascript', 'vbscript', 'expression', 
@@ -40,6 +41,10 @@ function xss(html){
 	})
 }
 
-var html = '<div onclick="alert(123)" title="32323"><script>alert(123)</script></div>';
-var result =  xss(html);
-console.log(result)
+vows.describe('xss-test').addBatch({
+	toStringCallback: () => {
+		var html = '<div onclick="alert(123)" title="32323"><script>alert(123)</script></div>';
+		var result =  xss(html);
+		assert.equal(result, '<div title="32323" xmlns="http://www.w3.org/1999/xhtml"></div>');
+	}
+}).export(module);
