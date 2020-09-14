b6b84a0cb8fb04ae78c7fe12e775536f95b7bdf7
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-09-14 16:29:19 -0400

    further minimize snapshots

--- test/xmltest/minimize-snapshot.js
+++ test/xmltest/minimize-snapshot.js
@@ -7,7 +7,7 @@ const {replaceNonTextChars} = require('xmltest');
  * @param actual {string | undefined | {toString: function(): string}}
  * @param errors {Partial<Record<ErrorLevel, string[]>>}
  * @param expected {string | undefined}
- * @returns {{actual: string} & Partial<Record<ErrorLevel, string[]>>}
+ * @returns {{actual?: string} & Partial<Record<ErrorLevel, string[]>>}
  */
 const minimizeSnapshot = (actual, errors, expected) => {
 	const actualOutput = replaceNonTextChars(actual);
@@ -15,7 +15,7 @@ const minimizeSnapshot = (actual, errors, expected) => {
 		? {}
 		: {actual: actualOutput};
 	const results = {...partial, ...errors};
-	return Object.keys(results).length === 0 ? {actual: actualOutput} : results;
+	return results;
 };
 
 module.exports = {
