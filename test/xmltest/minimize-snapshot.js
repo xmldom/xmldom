32d95039950a4c2c2155e659895356fc501abbcb
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-09-14 16:27:12 -0400

    rename parameter, etc.

--- test/xmltest/minimize-snapshot.js
+++ test/xmltest/minimize-snapshot.js
@@ -4,18 +4,18 @@ const {replaceNonTextChars} = require('xmltest');
  * Provides minimal representation by not adding empty lists or `expected`
  * to the snapshots.
  *
- * @param actualIn {string | undefined | {toString: function(): string}}
+ * @param actual {string | undefined | {toString: function(): string}}
  * @param errors {Partial<Record<ErrorLevel, string[]>>}
  * @param expected {string | undefined}
  * @returns {{actual: string} & Partial<Record<ErrorLevel, string[]>>}
  */
-const minimizeSnapshot = (actualIn, errors, expected) => {
-	const actual = replaceNonTextChars(actualIn);
+const minimizeSnapshot = (actual, errors, expected) => {
+	const actualOutput = replaceNonTextChars(actual);
 	const partial = expected && actual === expected
 		? {}
-		: {actual};
+		: {actual: actualOutput};
 	const results = {...partial, ...errors};
-	return Object.keys(results).length === 0 ? {actual} : results;
+	return Object.keys(results).length === 0 ? {actual: actualOutput} : results;
 };
 
 module.exports = {
