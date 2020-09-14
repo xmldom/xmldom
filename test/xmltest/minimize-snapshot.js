cc3ef75805f56fa8848b59c976b6137358b535d0
Christian Bewernitz <coder@karfau.de>
2020-09-14 23:54:08 +0200

    test: Restore adding `actual`, and `expected` when different
    This way tests fail if `actual` changes or even becomes different from or equal to `expected`.

--- test/xmltest/minimize-snapshot.js
+++ test/xmltest/minimize-snapshot.js
@@ -6,16 +6,19 @@ const {replaceNonTextChars} = require('xmltest');
  *
  * @param actual {string | undefined | {toString: function(): string}}
  * @param errors {Partial<Record<ErrorLevel, string[]>>}
- * @param expected {string | undefined}
+ * @param expected {string?} (optional) compared to actual and only added to output if different
  * @returns {{actual?: string} & Partial<Record<ErrorLevel, string[]>>}
  */
 const minimizeSnapshot = (actual, errors, expected) => {
 	const actualOutput = replaceNonTextChars(actual);
-	const partial = expected && actual === expected
-		? {}
-		: {actual: actualOutput};
-	const results = {...partial, ...errors};
-	return results;
+	const expectedOutput = replaceNonTextChars(expected);
+	const partial = {
+		actual: actualOutput
+	};
+	if (expectedOutput && actualOutput !== expectedOutput) {
+		partial.expected = expectedOutput
+	}
+	return {...partial, ...errors};
 };
 
 module.exports = {
