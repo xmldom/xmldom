72e2ae24b5fcdf43142f55c280d961d920d771e8
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-09-15 12:41:21 -0400

    fix Prettier formatting in test

--- test/dom-parser.test.js
+++ test/dom-parser.test.js
@@ -1,49 +1,47 @@
-const {DOMParser} = require('../lib/dom-parser');
+const { DOMParser } = require('../lib/dom-parser');
 
 describe('DOMParser', () => {
 	describe('constructor', () => {
 		test('should store passed options.locator', () => {
-			const options = {locator: {}};
+			const options = { locator: {} };
 			const it = new DOMParser(options);
 
 			// TODO: is there a simpler way to test this that doesn't involve invoking parseFromString?
-			it.parseFromString('<xml/>')
+			it.parseFromString('<xml/>');
 
 			const expected = {
 				columnNumber: 1,
 				lineNumber: 1,
 			};
 			expect(options.locator).toEqual(expected);
-		})
+		});
 
 		test('should store passed options.xmlns for default mime type', () => {
-			const options = {xmlns: {'': 'custom-default-ns'}};
+			const options = { xmlns: { '': 'custom-default-ns' } };
 			const it = new DOMParser(options);
 
 			// TODO: is there a simpler way to test this that doesn't involve invoking parseFromString?
-			const actual = it.parseFromString('<xml/>')
+			const actual = it.parseFromString('<xml/>');
 
-			expect(actual.toString()).toEqual('<xml xmlns="custom-default-ns"/>')
-		})
+			expect(actual.toString()).toEqual('<xml xmlns="custom-default-ns"/>');
+		});
 
 		test('should store and modify passed options.xmlns for html mime type', () => {
-			const options = {xmlns: {'': 'custom-default-ns'}};
+			const options = { xmlns: { '': 'custom-default-ns' } };
 			const it = new DOMParser(options);
 
 			// TODO: is there a simpler way to test this that doesn't involve invoking parseFromString?
-			it.parseFromString('<xml/>', 'text/html')
+			it.parseFromString('<xml/>', 'text/html');
 
-			expect(options.xmlns['']).toEqual('http://www.w3.org/1999/xhtml')
-		})
-	})
+			expect(options.xmlns['']).toEqual('http://www.w3.org/1999/xhtml');
+		});
+	});
 
 	describe('parseFromString', () => {
 		test('should use minimal entity map for default mime type', () => {
-			const XML = '<xml attr="&quot;">&lt; &amp;</xml>'
-			expect(
-				new DOMParser().parseFromString(XML).toString()
-			).toEqual(XML)
-		})
+			const XML = '<xml attr="&quot;">&lt; &amp;</xml>';
+			expect(new DOMParser().parseFromString(XML).toString()).toEqual(XML);
+		});
 
 		test('should provide access to textContent and attribute values', () => {
 			// provides an executable example for https://github.com/xmldom/xmldom/issues/93
@@ -68,13 +66,13 @@ describe('DOMParser', () => {
 			*/
 			const textTags = document.getElementsByTagName('text');
 
-			expect(textTags.length).toEqual(3)
+			expect(textTags.length).toEqual(3);
 			const expectedText = ['first', 'second', 'last'];
 			for (let i = 0; i < textTags.length; i++) {
 				const textTag = textTags[i];
 				expect(textTag.textContent).toEqual(expectedText[i]);
 				expect(textTag.getAttribute('top')).toEqual(`${i}`);
 			}
-		})
-	})
-})
+		});
+	});
+});
