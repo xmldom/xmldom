581ad93b827522f0434b513317e7a25df94a8bff
Christian Bewernitz <coder@karfau.de>
2020-09-16 02:12:20 +0200

    test: Add jest testsuite with `xmltest` cases (#112)
    The only thing the current test suite does is to run tons of cases and take a snapshot of the output and errors,
    so that hopefully a test will fail if we make a breaking change.
    
    (It doesn't cover breaking API changes of course, but sets the basis and any added test will be part of CI testing.)
    
    Add devDependencies:
    - npm.im/xmltest from https://github.com/karfau/xmltest & it's peerDependencies:
      - npm.im/get-stream
      - npm.im/yauzl
    
    Add new helper module `test/get-test-parser.js`:
    To simplify the very common use case of testing DOMParser results
    
    Co-authored-by: Christopher J. Brody <chris.brody+brodybits@gmail.com>

---
+++ test/xmltest/generate-snapshot.js
@@ -0,0 +1,26 @@
+const { replaceNonTextChars } = require('xmltest')
+
+/**
+ * Generate minimal snapshot representation by not adding empty lists to the
+ * snapshots.
+ *
+ * @param actual {string | undefined | {toString: function(): string}}
+ * @param errors {Partial<Record<ErrorLevel, string[]>>}
+ * @param expected {string?} (optional) compared to actual-only added to output if different
+ * @returns {{actual?: string} & Partial<Record<ErrorLevel, string[]>>}
+ */
+const generateSnapshot = (actual, errors, expected) => {
+	const actualForSnapshot = replaceNonTextChars(actual)
+	const expectedForSnapshot = replaceNonTextChars(expected)
+	const partial = {
+		actual: actualForSnapshot
+	}
+	if (expectedForSnapshot && actualForSnapshot !== expectedForSnapshot) {
+		partial.expected = expectedForSnapshot
+	}
+	return { ...partial, ...errors }
+}
+
+module.exports = {
+	generateSnapshot
+}
