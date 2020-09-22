efc2c147a5b9ce14fea3ef5ba73143cf576e0d2c
Christian Bewernitz <coder@karfau.de>
2020-09-22 21:18:27 +0200

    refactor: Switch `error/xml-error.test.js`

--- test/error/xml-error.test.js
+++ test/error/xml-error.test.js
@@ -1,83 +1,54 @@
 'use strict'
 
-var DOMParser = require('../../lib/dom-parser').DOMParser
-var assert = require('../assert')
+const { getTestParser } = require('../get-test-parser')
 
 describe('errorHandle', () => {
-	it('empty document', () => {
-		var errors = []
-		var p = new DOMParser({
-			errorHandler: function (key, msg) {
-				errors.push(key, msg)
-			},
-		})
-		var dom = p.parseFromString('', 'text/xml')
-		assert(errors.length, 2)
-	})
-
 	it('unclosed document', () => {
-		var errors = []
-		var p = new DOMParser({
-			errorHandler: function (key, msg) {
-				errors.push(key, msg)
-			},
-		})
-		var dom = p.parseFromString('<img>', 'text/xml')
-		assert(errors.length, 2)
+		const { errors, parser } = getTestParser()
+
+		const actual = parser.parseFromString('<img>', 'text/xml').toString()
+
+		expect({ actual, ...errors }).toMatchSnapshot()
 	})
 
-	it('unclosed hmtl tags', () => {
-		var errors = []
-		var p = new DOMParser({
-			errorHandler: function (key, msg) {
-				errors.push(key, msg)
-			},
-		})
-		var dom = p.parseFromString('<img>', 'text/html')
-		assert(errors.length, 0, 'unclosed html tag not need report!!')
+	it('unclosed html tag', () => {
+		const { errors, parser } = getTestParser()
+
+		const actual = parser.parseFromString('<img>', 'text/html').toString()
+
+		expect({ actual, ...errors }).toMatchSnapshot()
 	})
 
-	it('invalid xml node', () => {
-		var errors = []
-		var p = new DOMParser({
-			errorHandler: function (key, msg) {
-				errors.push(key, msg)
-			},
-		})
-		assert.equal(
-			p.parseFromString('<test><!--', 'text/xml').documentElement + '',
-			'<test/>'
-		)
-		assert(errors.length, 4)
-		errors = []
-		assert.equal(
-			p.parseFromString('<r', 'text/xml').documentElement + '',
-			'<r/>'
-		)
-		assert(errors.length, 4)
+	it.each([
+		['<test><!--', '<test/>'],
+		['<r', '<r/>'],
+	])('invalid xml node "%s"', (input, expected) => {
+		const { errors, parser } = getTestParser()
+
+		const actual = parser
+			.parseFromString(input, 'text/xml')
+			.documentElement.toString()
+
+		expect({ actual, ...errors }).toMatchSnapshot({ actual: expected })
 	})
 
-	it('invalid html attribute (miss quote)', () => {
-		var errors = []
-		var p = new DOMParser({
-			errorHandler: function (key, msg) {
-				errors.push(key, msg)
-			},
-		})
-		var dom = p.parseFromString('<img attr=1/>', 'text/html')
-		assert(errors.length, 2, 'invalid xml attribute(miss qute)')
-		assert(dom + '', '<img attr="1" xmlns="http://www.w3.org/1999/xhtml"/>')
+	it('html attribute (miss quote)', () => {
+		const { errors, parser } = getTestParser()
+
+		const actual = parser
+			.parseFromString('<img attr=1/>', 'text/html')
+			.toString()
+
+		expect({ actual, ...errors }).toMatchSnapshot()
 	})
 
 	it('valid html attribute value (<>&)', () => {
-		var dom = new DOMParser({}).parseFromString(
-			'<img attr="<>&"/>',
-			'text/html'
-		)
-		assert(
-			dom + '',
-			'<img attr="&lt;>&amp;" xmlns="http://www.w3.org/1999/xhtml"/>',
-			'invalid xml attribute valus (<)'
-		)
+		const { errors, parser } = getTestParser()
+
+		const actual = parser
+			.parseFromString('<img attr="<>&"/>', 'text/html')
+			.toString()
+
+		expect({ actual, ...errors }).toMatchSnapshot()
 	})
 })
