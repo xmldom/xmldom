8604b6658912659c247f649f01ea16d5a7fcb22b
Christian Bewernitz <coder@karfau.de>
2020-09-24 07:54:32 +0200

    less redundant tests in `normalize.test.js`

--- test/html/normalize.test.js
+++ test/html/normalize.test.js
@@ -5,7 +5,6 @@ const { getTestParser } = require('../get-test-parser')
 describe('html normalizer', () => {
 	it.each([
 		'<div>&amp;&lt;123&456<789;&&</div>',
-		'<div>&amp;&lt;123&456<789;&&</div>',
 		'<div><123e>&<a<br/></div>',
 		'<div>&nbsp;&copy;&nbsp&copy</div>',
 		'<html xmlns:x="1"><body/></html>',
@@ -31,7 +30,6 @@ describe('html normalizer', () => {
 		'<html title= 1/>',
 		'<html title = 1/>',
 		'<html title/>',
-		'<html><meta><link><img><br><hr><input></html>',
 	])('unclosed html %s', (xml) => {
 		const { errors, parser } = getTestParser()
 
@@ -69,6 +67,23 @@ describe('html normalizer', () => {
 			// For the future, it may be nicer to use \uxxxx in the assert strings
 			// rather than pasting in multi-byte UTF-8 Unicode characters
 			actual: '<div xmlns="http://www.w3.org/1999/xhtml">ÄäÅåÆæÖöØøßÜü€</div>',
+		})
+	})
+	it('European entities xml', () => {
+		const { errors, parser } = getTestParser()
+
+		const actual = parser
+			.parseFromString(
+				'<div>&Auml;&auml;&Aring;&aring;&AElig;&aelig;&Ouml;&ouml;&Oslash;&oslash;&szlig;&Uuml;&uuml;&euro;</div>',
+				'text/xml'
+			)
+			.toString()
+
+		expect({ actual, ...errors }).toMatchObject({
+			// For the future, it may be nicer to use \uxxxx in the assert strings
+			// rather than pasting in multi-byte UTF-8 Unicode characters
+			actual:
+				'<div>&amp;Auml;&amp;auml;&amp;Aring;&amp;aring;&amp;AElig;&amp;aelig;&amp;Ouml;&amp;ouml;&amp;Oslash;&amp;oslash;&amp;szlig;&amp;Uuml;&amp;uuml;&amp;euro;</div>',
 		})
 	})
 })
