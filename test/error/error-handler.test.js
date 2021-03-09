d4201b9dfbf760049f457f9f08a3888d48835135
Chris Brody <chris.brody+brodybits@gmail.com>
2021-03-08 22:21:56 -0500

    Merge pull request from GHSA-h6q6-9hqw-rwfv
    * fix!: Preserve quotes in DOCTYPE declaration
    
    Since the only purpose of parsing the DOCTYPE is to be able to restore it when serializing,
    we decided that it would be best to leave the parsed publicId and systemId as is,
    including any quotes.
    
    BREAKING CHANGE: If somebody relies on the actual unquoted values of those ids,
    they will need to take care of either single or double quotes and the right escaping.
    
    (Without this change this would not have been possible because the SAX parser already
    dropped the information about the quotes that have been used in the source.)
    
    https://www.w3.org/TR/2006/REC-xml11-20060816/#dtd
    https://www.w3.org/TR/2006/REC-xml11-20060816/#IDAX1KS (External Entity Declaration)
    
    Co-authored-by: Christian Bewernitz <coder@karfau.de>
    Co-authored-by: Chris Brody <chris.brody+brodybits@gmail.com>
    
    * feat(security): Improve error reporting; throw on duplicate attribute
    
    BREAKING CHANGE: It is currently not clear how to consistently deal with duplicate attributes,
    so it is also safer for our users to fail when detecting them.
    
    It is possible to configure the `DOMParser.errorHandler` before parsing, to handle those errors differently.
    
    To accomplish this and also be able to verify it in tests we needed to:
    
    - create a new `Error` type `ParseError` and export it
    - Throw `ParseError` from `errorHandler.fatalError` and prevent those from being caught in `XMLReader`.
    - export `DOMHandler` constructor as `__DOMHandler`
    
    Co-authored-by: Christian Bewernitz <coder@karfau.de>
    Co-authored-by: Chris Brody <chris.brody+brodybits@gmail.com>
    
    Co-authored-by: Christian Bewernitz <coder@karfau.de>

---
+++ test/error/error-handler.test.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const { DOMParser } = require('../../lib/dom-parser')
+const { REPORTED } = require('./reported')
+
+describe('custom errorHandler', () => {
+	it('function with two args receives key and message', () => {
+		const errors = {}
+		const parser = new DOMParser({
+			// currently needs to be a `function` to make the test work,
+			// `jest.fn()` or using `() => {}` doesn't work
+			errorHandler: function (key, msg) {
+				errors[key] = msg
+			},
+		})
+
+		parser.parseFromString(REPORTED.WF_AttributeMissingQuote.source, 'text/xml')
+		expect(errors).toHaveProperty('warning')
+		parser.parseFromString(
+			REPORTED.SYNTAX_AttributeEqualMissingValue.source,
+			'text/xml'
+		)
+		expect(errors).toHaveProperty('error')
+		parser.parseFromString(REPORTED.WF_DuplicateAttribute.source, 'text/xml')
+		expect(errors).toHaveProperty('fatalError')
+	})
+
+	it('function with one argument builds list', () => {
+		const errors = []
+		const parser = new DOMParser({
+			// currently needs to be a `function` to make the test work,
+			// `jest.fn()` or using `() => {}` doesn't work
+			errorHandler: function (msg) {
+				errors.push(msg)
+			},
+		})
+
+		parser.parseFromString(REPORTED.WF_AttributeMissingQuote.source, 'text/xml')
+		parser.parseFromString(
+			REPORTED.SYNTAX_AttributeEqualMissingValue.source,
+			'text/xml'
+		)
+		parser.parseFromString(REPORTED.WF_DuplicateAttribute.source, 'text/xml')
+
+		expect(errors).toMatchObject([
+			/\[xmldom warning]/,
+			/\[xmldom error]/,
+			/\[xmldom fatalError]/,
+		])
+	})
+})
