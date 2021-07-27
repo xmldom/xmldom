c4a58d5113532b72c7f68cb3a47db3c818e236ef
Christian Bewernitz <coder@karfau.de>
2021-07-27 22:32:03 +0200

    fix: Correct Misinterpretation of malicious XML input
    resolves https://github.com/xmldom/xmldom/security/advisories/GHSA-5fg8-2547-mr8q
    
    (cherry picked from commit 7b4b743917a892d407356e055b296dcd6d107e8b)

--- test/dom/serializer.test.js
+++ test/dom/serializer.test.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const { DOMParser } = require('../../lib/dom-parser')
+const { XMLSerializer } = require('../../lib/dom')
 
 describe('XML Serializer', () => {
 	it('supports text node containing "]]>"', () => {
@@ -16,5 +17,24 @@ describe('XML Serializer', () => {
 		expect(doc.documentElement.firstChild.toString()).toBe(
 			'<script xmlns="http://www.w3.org/1999/xhtml"></script>'
 		)
+	})
+	describe('properly escapes attribute values', () => {
+		it('should escape special characters in namespace attributes', () => {
+			const input = `<xml xmlns='<&"' xmlns:attr='"&<'><test attr:test=""/></xml>`
+			const doc = new DOMParser().parseFromString(input, 'text/xml')
+
+			// in this case the explicit attribute nodes are serialized
+			expect(new XMLSerializer().serializeToString(doc)).toBe(
+				'<xml xmlns="&lt;&amp;&quot;" xmlns:attr="&quot;&amp;&lt;"><test attr:test=""/></xml>'
+			)
+
+			// in this case the namespace attributes are "inherited" from the parent,
+			// which is not serialized
+			expect(
+				new XMLSerializer().serializeToString(doc.documentElement.firstChild)
+			).toBe(
+				'<test xmlns:attr="&quot;&amp;&lt;" attr:test="" xmlns="&lt;&amp;&quot;"/>'
+			)
+		})
 	})
 })
