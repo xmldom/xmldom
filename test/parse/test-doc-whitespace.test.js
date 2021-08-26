d5405e48a56e22c33ce35565500f8451dfbe5155
Christian Bewernitz <coder@karfau.de>
2021-08-26 09:13:06 +0200

    fix: Normalize line breaks according to spec
    > XML parsed entities are often stored in computer files which, for editing convenience, are organized into lines. These lines are typically separated by some combination of the characters CARRIAGE RETURN (#xD) and LINE FEED (#xA).
    >
    > To simplify the tasks of applications, the XML processor must behave as if it normalized all line breaks in external parsed entities (including the document entity) on input, before parsing, by translating both the two-character sequence #xD #xA and any #xD that is not followed by #xA to a single #xA character.
    
    Where `#xD` == `\r` and `#xA` == `\n`, so
    ` \r\n ` => ` \n `
    ` \n\r ` => ` \n\n `
    ` \n  ` => ` \n `
    ` \r  ` => ` \n `
    
    BREAKING CHANGE: Certain combination of line break characters are normalized before parsing takes place and will no longer be preserved. For details see https://www.w3.org/TR/xml/#sec-line-ends
    
    fixes https://github.com/xmldom/xmldom/issues/303

--- test/parse/test-doc-whitespace.test.js
+++ test/parse/test-doc-whitespace.test.js
@@ -20,12 +20,71 @@ describe('errorHandle', () => {
 		expect({ actual, ...errors }).toMatchSnapshot()
 	})
 
-	it('test', () => {
+	it('should encode < literal when not part of a tag', () => {
 		const description = '<p>populaciji (< 0.1%), te se</p>'
 		const { errors, parser } = getTestParser()
 
 		const actual = parser.parseFromString(description, 'text/html').toString()
 
 		expect({ actual, ...errors }).toMatchSnapshot()
+	})
+})
+
+describe('whitespace', () => {
+	const whitespaceToHex = (str) =>
+		str.replace(/\s/g, (c) => `#x${c.charCodeAt(0).toString(16)}`)
+	it.each([
+		[
+			'in text node before first element',
+			'\r\n<xml/>',
+			(dom) => dom.firstChild.nodeValue,
+			'#xa',
+		],
+		[
+			'in attributes',
+			'<xml attr="\r\n"/>',
+			(dom) => dom.documentElement.getAttribute('attr'),
+			'#xa',
+		],
+		[
+			'in firstChild text node',
+			'<xml>\x0D\x0A</xml>',
+			(dom) => dom.documentElement.firstChild.nodeValue,
+			'#xa',
+		],
+	])('should normalize "\\r\\n" %s', (_, xml, resolveNode, expected) => {
+		const { parser } = getTestParser()
+
+		const dom = parser.parseFromString(xml, 'text/html')
+
+		expect(whitespaceToHex(resolveNode(dom))).toBe(expected)
+	})
+	it.each([
+		[
+			'before first node',
+			'\r \n<xml/>',
+			(dom) => dom.firstChild.nodeValue,
+			'#xa#x20#xa',
+		],
+		[
+			'in attributes',
+			'<xml attr=" \r\r"/>',
+			(dom) => dom.documentElement.getAttribute('attr'),
+			'#x20#xa#xa',
+		],
+		[
+			'in firstChild text node',
+			'<xml>\x0A\x0D</xml>',
+			(dom) => dom.documentElement.firstChild.nodeValue,
+			// only the "inner" combination is replaced, which results in a new combination
+			// (which would be normalized on the next roundtrip)
+			'#xa#xa',
+		],
+	])('should normalize "\\r" not followed by "\\n" %s', (_, xml, resolveNode, expected) => {
+		const { parser } = getTestParser()
+
+		const dom = parser.parseFromString(xml, 'text/html')
+
+		expect(whitespaceToHex(resolveNode(dom))).toBe(expected)
 	})
 })
