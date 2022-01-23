4f65b787fba65b28a38727e22c71ec9a82becaf7
Niklas Lindström <lindstream@gmail.com>
2022-01-23 18:27:15 +0100

    Update tests to reflect added DOCTYPE processing
    This reflects that the internal XML entities are now parsed and
    expanded, and that an empty DOCTYPE is output.
    
    Although the xmltest specs (from James Clark) do not contain these empty
    DOCTYPE declarations, apparently Firefox and Chromium-based web browsers
    do.

--- test/xmltest/valid.test.js
+++ test/xmltest/valid.test.js
@@ -13,11 +13,7 @@ describe('xmltest/valid', () => {
 
 		Object.entries(entries).forEach(([pathInZip, filename]) => {
 			test(`should match ${filename} with snapshot`, async () => {
-				const input = (await xmltest.getContent(pathInZip))
-					// TODO: The DOCTYPE totally confuses xmldom :sic:
-					// for now we remove it and any newlines after it so we have reasonable tests
-					.replace(/^<!DOCTYPE doc \[[^\]]+]>[\r\n]*/m, '')
-
+				const input = await xmltest.getContent(pathInZip)
 				const expected = await xmltest.getContent(
 					xmltest.RELATED.out(pathInZip)
 				)
