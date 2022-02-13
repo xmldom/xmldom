2f90b45e6c2741648205671707499903889f21db
Christian Bewernitz <coder@karfau.de>
2022-02-13 21:06:40 +0100

    fix(sax): Only use own properties in entityMap
    Fixes #370

---
+++ test/parse/entities.test.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const { getTestParser } = require('../get-test-parser')
+const { MIME_TYPE } = require('../../lib/conventions')
+
+describe('entity replacement ignores js prototype chain', () => {
+	it("should not pick up 'entities' from the prototype chain", () => {
+		const source = `
+		<xml>
+			<hasOwnProperty>&hasOwnProperty;</hasOwnProperty> 
+			<proto>&__proto__;</proto> 
+			<constructor>&constructor;</constructor>
+		</xml>
+`
+		const { errors, parser } = getTestParser()
+
+		const actual = parser.parseFromString(source, MIME_TYPE.XML_TEXT).toString()
+
+		expect({ actual, ...errors }).toMatchSnapshot()
+	})
+})
