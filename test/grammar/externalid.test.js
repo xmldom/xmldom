e02d69df5bd9cc6f5536c4dba9d8a7f9596caf2f
Christian Bewernitz <coder@karfau.de>
2023-07-01 16:04:32 +0200

    test: add tests for NotationDecl

--- test/grammar/externalid.test.js
+++ test/grammar/externalid.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { describe, expect, test } = require('@jest/globals');
-const { ExternalID, PubidLiteral, S, SystemLiteral, reg } = require('../../lib/grammar');
+const { ExternalID, PubidLiteral, S, SystemLiteral, reg, NotationDecl, Name } = require('../../lib/grammar');
 const { range } = require('./utils');
 
 describe('SystemLiteral', () => {
@@ -38,6 +38,19 @@ describe('PubidLiteral', () => {
 	);
 });
 
+const VALID_SYSTEM = [`SYSTEM ""`, `SYSTEM ''`, `SYSTEM "'"`, `SYSTEM '"'`];
+const VALID_PUBLIC_DOUBLE = [
+	`PUBLIC "" ""`,
+	`PUBLIC '' ""`,
+	`PUBLIC "" ''`,
+	`PUBLIC '' ''`,
+	`PUBLIC "'" "'"`,
+	`PUBLIC '' "'"`,
+	`PUBLIC "'" '"'`,
+	`PUBLIC '' '"'`,
+	`PUBLIC "\x20\x0D\x0Aa-zA-Z0-9-()+,./:=?;!*#@$_%" '"'`,
+	`PUBLIC '\x20\x0D\x0Aa-zA-Z0-9-()+,./:=?;!*#@$_%' '"'`,
+];
 describe('ExternalID', () => {
 	test('should contain SystemLiteral twice', () => {
 		expect(ExternalID.source.split(SystemLiteral.source)).toHaveLength(3);
@@ -46,7 +59,7 @@ describe('ExternalID', () => {
 		expect(ExternalID.source.split(PubidLiteral.source)).toHaveLength(2);
 	});
 	describe('SYSTEM', () => {
-		[`SYSTEM ""`, `SYSTEM ''`, `SYSTEM "'"`, `SYSTEM '"'`].forEach((valid) =>
+		VALID_SYSTEM.forEach((valid) =>
 			test(`should match ${valid}`, () => {
 				expect(ExternalID.exec(valid)[0]).toBe(valid);
 			})
@@ -58,18 +71,7 @@ describe('ExternalID', () => {
 		);
 	});
 	describe('PUBLIC', () => {
-		[
-			`PUBLIC "" ""`,
-			`PUBLIC '' ""`,
-			`PUBLIC "" ''`,
-			`PUBLIC '' ''`,
-			`PUBLIC "'" "'"`,
-			`PUBLIC '' "'"`,
-			`PUBLIC "'" '"'`,
-			`PUBLIC '' '"'`,
-			`PUBLIC "\x20\x0D\x0Aa-zA-Z0-9-()+,./:=?;!*#@$_%" '"'`,
-			`PUBLIC '\x20\x0D\x0Aa-zA-Z0-9-()+,./:=?;!*#@$_%' '"'`,
-		].forEach((valid) =>
+		VALID_PUBLIC_DOUBLE.forEach((valid) =>
 			test(`should match ${valid}`, () => {
 				expect(ExternalID.exec(valid)[0]).toBe(valid);
 			})
@@ -128,4 +130,43 @@ describe('ExternalID', () => {
 		})
 	);
 */
+});
+
+describe('NotationDecl', () => {
+	test('should contain S Name S once', () => {
+		expect(NotationDecl.source.split(reg(S, Name, S).source)).toHaveLength(2);
+	});
+	test('should contain ExternalID once', () => {
+		expect(NotationDecl.source.split(ExternalID.source)).toHaveLength(2);
+	});
+	test('should contain PubidLiteral twice', () => {
+		// the first is inside ExternalID
+		// the second is in PublicID
+		expect(NotationDecl.source.split(PubidLiteral.source)).toHaveLength(3);
+	});
+	const VALID_PUBLIC_SINGLE = VALID_PUBLIC_DOUBLE.map((pub) => {
+		// strip second air of quotes from pub
+		return pub.substring(0, pub.lastIndexOf(' '));
+	})
+		.filter((pub, i, all) => {
+			// remove duplicates (only accepting the first occurrence)
+			return all.indexOf(pub) === i;
+		})
+		.map((pub) => `<!NOTATION Name ${pub}>`);
+	test('should have VALID_PUBLIC_SINGLE testcases', () => {
+		expect(VALID_PUBLIC_SINGLE).toHaveLength(5);
+	});
+	[
+		...VALID_SYSTEM.map((sys) => `<!NOTATION Name ${sys}>`),
+		...VALID_PUBLIC_DOUBLE.map((pub) => `<!NOTATION Name ${pub}>`),
+		...VALID_PUBLIC_SINGLE,
+	].forEach((valid) =>
+		test(`should match ${valid}`, () => {
+			expect(NotationDecl.exec(valid)[0]).toBe(valid);
+		})
+	);
+	test('should accept all spaces in all places', () => {
+		const source = `<!NOTATION\n \r\tName\n \r\tPUBLIC ""\n \r\t>`;
+		expect(NotationDecl.exec(source)[0]).toBe(source);
+	});
 });
