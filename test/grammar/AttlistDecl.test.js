d51a074adfdf1914b5cc00da359a1d80b9795b09
Christian Bewernitz <coder@karfau.de>
2023-07-02 16:01:19 +0200

    test: add tests for AttlistDecl

---
+++ test/grammar/AttlistDecl.test.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const { describe, expect, test } = require('@jest/globals');
+const { AttlistDecl, S, Name, S_OPT } = require('../../lib/grammar');
+
+describe('AttlistDecl', () => {
+	test('should contain Name six times', () => {
+		expect(AttlistDecl.source.split(Name.source)).toHaveLength(7);
+	});
+	test('should contain S six times', () => {
+		expect(AttlistDecl.source.split(S.source)).toHaveLength(7);
+	});
+	test('should contain S_OPT seven times', () => {
+		expect(AttlistDecl.source.split(S_OPT.source)).toHaveLength(10);
+	});
+	[
+		`<!ATTLIST target Name CDATA "">`,
+		`<!ATTLIST target Name ID #REQUIRED >`,
+		`<!ATTLIST target Name ID 'AttValue'>`,
+		`<!ATTLIST target Name IDREF '&EntityRef;&#123;&#x0A; AttValue'>`,
+		`<!ATTLIST target Name IDREFS #IMPLIED >`,
+		`<!ATTLIST target Name ENTITY #FIXED 'AttValue'>`,
+		`<!ATTLIST target Name ENTITIES 'AttValue'>`,
+		`<!ATTLIST target Name NMTOKEN 'AttValue'>`,
+		`<!ATTLIST target Name NMTOKENS 'AttValue'>`,
+		`<!ATTLIST target Name (Enumeration|NmToken) 'AttValue'>`,
+		`<!ATTLIST target Name NOTATION (Name|Name) 'AttValue'>`,
+	].forEach((valid) =>
+		test(`should match different AttType and AttValue ${valid}`, () => {
+			expect(AttlistDecl.exec(valid)[0]).toBe(valid);
+		})
+	);
+	[
+		`<!ATTLIST termdef
+id      ID      #REQUIRED
+name    CDATA   #IMPLIED>`,
+		`<!ATTLIST list type (bullets|ordered|glossary) "ordered">`,
+		`<!ATTLIST form method  CDATA   #FIXED "POST">`,
+		`<!ATTLIST poem xml:space (default|preserve) 'preserve'>`,
+		`<!ATTLIST pre xml:space (preserve) #FIXED 'preserve'>`,
+		`<!ATTLIST target xml:lang CDATA #IMPLIED>`,
+		`<!ATTLIST poem xml:lang CDATA 'fr'>`,
+		`<!ATTLIST gloss xml:lang CDATA 'en'>`,
+		`<!ATTLIST note xml:lang CDATA 'en'>`,
+	].forEach((valid) =>
+		test(`should match examples from spec ${valid}`, () => {
+			expect(AttlistDecl.exec(valid)[0]).toBe(valid);
+		})
+	);
+	[`<!ATTLIST\n \r\tpoem\n \r\txml:space\n \r\t(default|preserve)\n \r\t'preserve'\n \r\t>`].forEach((valid) =>
+		test('should accept all spaces in all places', () => {
+			expect(AttlistDecl.exec(valid)[0]).toBe(valid);
+		})
+	);
+});
--- test/grammar/externalid.test.js
+++ test/grammar/externalid.test.js
@@ -82,54 +82,6 @@ describe('ExternalID', () => {
 			})
 		);
 	});
-	/*
-	[
-		`<!DOCTYPE name PUBLIC "identifier"  "url" [
-		<?foo "foo"?>
-		<!ENTITY bar "bar">
-		<!ENTITY bar "bar2"> <!--  -->
-		<!ENTITY % baz "baz"> %test;
-		]>`,
-		`<!DOCTYPE name>`,
-		`<!DOCTYPE name SYSTEM "identifier" [
-		  <!ENTITY foo "foo">
-		  <!ENTITY bar "bar">
-		  <!ENTITY bar "bar2"> <!--  -->
-		  <!ENTITY % baz "baz"> %test;
-		]>`,
-		`<!DOCTYPE name [
-		 <!ELEMENT br EMPTY>
-		 <!ELEMENT p (#PCDATA|emph)* >
-		 <!ELEMENT container ANY>
-		 <!ELEMENT %name.para; %content.para; >
-		 ]>`,
-		`<!DOCTYPE HTML >`,
-		`<!DOCTYPE greeting SYSTEM "hello.dtd">`,
-		`<!DOCTYPE name PUBLIC '' ''>`,
-		`<!DOCTYPE name SYSTEM 'test' []  >`,
-		`<!DOCTYPE name PUBLIC 'test' 'test2'>`,
-		`<!DOCTYPE name PUBLIC 'test' "tes't2">`,
-	].forEach((source, index) =>
-		//? source
-		test(`should match valid doctype at index ${index}`, () => {
-			expect(doctypedecl.test(source)).toBe(true);
-		})
-	);
-	[
-		`<!DOCTYPE x PUBLIC '"'>`,
-		`<!DOCTYPE SYSTEM "bla">`,
-		`<!DOCTYPE name [%;]>`,
-		`<!DOCTYPE name SYSTEM 'test' 'test2'>`,
-		`<!DOCTYPE name PUBLIC 'te'st' 'test2'>`,
-		`<!DOCTYPE name PUBLIC 'test' 'tes't2'>`,
-		`<!DOCTYPE name PUBLIC 'test' "tes"t2">`,
-	].forEach((source, index) =>
-		//? source
-		test(`should not match invalid doctype at index ${index}`, () => {
-			expect(doctypedecl.test(source)).toBe(false);
-		})
-	);
-*/
 });
 
 describe('NotationDecl', () => {
