eac02bd53c518c31c48b47278375a80008f66d35
Chris Barth <chrisjbarth@hotmail.com>
2023-07-12 12:57:05 -0400

    format

--- test/dom/processing-instruction.test.js
+++ test/dom/processing-instruction.test.js
@@ -5,44 +5,44 @@ const { MIME_TYPE } = require('../../lib/conventions');
 const { Node } = require('../../lib/dom');
 
 describe('ProcessingInstruction', () => {
-    test('can properly create a ProcessingInstruction', () => {
-        const doc = new DOMParser().parseFromString('<xml></xml>', MIME_TYPE.XML_TEXT);
-        const pi = doc.createProcessingInstruction('xml-stylesheet', 'href="mycss.css" type="text/css"');
+	test('can properly create a ProcessingInstruction', () => {
+		const doc = new DOMParser().parseFromString('<xml></xml>', MIME_TYPE.XML_TEXT);
+		const pi = doc.createProcessingInstruction('xml-stylesheet', 'href="mycss.css" type="text/css"');
 
-        expect(pi.target).toBe('xml-stylesheet');
-        expect(pi.data).toBe('href="mycss.css" type="text/css"');
-        expect(pi.nodeType).toBe(Node.PROCESSING_INSTRUCTION_NODE);
-        expect(pi.nodeName).toBe('xml-stylesheet');
-    });
+		expect(pi.target).toBe('xml-stylesheet');
+		expect(pi.data).toBe('href="mycss.css" type="text/css"');
+		expect(pi.nodeType).toBe(Node.PROCESSING_INSTRUCTION_NODE);
+		expect(pi.nodeName).toBe('xml-stylesheet');
+	});
 
-    test('can properly append a ProcessingInstruction to a document', () => {
-        const doc = new DOMParser().parseFromString('<xml></xml>', MIME_TYPE.XML_TEXT);
-        const pi = doc.createProcessingInstruction('xml-stylesheet', 'href="mycss.css" type="text/css"');
+	test('can properly append a ProcessingInstruction to a document', () => {
+		const doc = new DOMParser().parseFromString('<xml></xml>', MIME_TYPE.XML_TEXT);
+		const pi = doc.createProcessingInstruction('xml-stylesheet', 'href="mycss.css" type="text/css"');
 
-        doc.appendChild(pi);
-        const str = new XMLSerializer().serializeToString(doc).trim();
+		doc.appendChild(pi);
+		const str = new XMLSerializer().serializeToString(doc).trim();
 
-        expect(str).toBe('<xml/><?xml-stylesheet href="mycss.css" type="text/css"?>');
-    });
+		expect(str).toBe('<xml/><?xml-stylesheet href="mycss.css" type="text/css"?>');
+	});
 
-    test('can properly clone a ProcessingInstruction', () => {
-        const doc = new DOMParser().parseFromString('<xml></xml>', MIME_TYPE.XML_TEXT);
-        const pi = doc.createProcessingInstruction('xml-stylesheet', 'href="mycss.css" type="text/css"');
+	test('can properly clone a ProcessingInstruction', () => {
+		const doc = new DOMParser().parseFromString('<xml></xml>', MIME_TYPE.XML_TEXT);
+		const pi = doc.createProcessingInstruction('xml-stylesheet', 'href="mycss.css" type="text/css"');
 
-        doc.appendChild(pi);
-        const piClone = pi.cloneNode();
+		doc.appendChild(pi);
+		const piClone = pi.cloneNode();
 
-        expect(piClone.target).toBe(pi.target);
-        expect(piClone.data).toBe(pi.data);
-        expect(piClone.nodeType).toBe(pi.nodeType);
-        expect(piClone.nodeName).toBe(pi.nodeName);
-    });
+		expect(piClone.target).toBe(pi.target);
+		expect(piClone.data).toBe(pi.data);
+		expect(piClone.nodeType).toBe(pi.nodeType);
+		expect(piClone.nodeName).toBe(pi.nodeName);
+	});
 
-    test('can properly set data of a ProcessingInstruction', () => {
-        const doc = new DOMParser().parseFromString('<xml></xml>', MIME_TYPE.XML_TEXT);
-        const pi = doc.createProcessingInstruction('xml-stylesheet', 'href="mycss.css" type="text/css"');
+	test('can properly set data of a ProcessingInstruction', () => {
+		const doc = new DOMParser().parseFromString('<xml></xml>', MIME_TYPE.XML_TEXT);
+		const pi = doc.createProcessingInstruction('xml-stylesheet', 'href="mycss.css" type="text/css"');
 
-        pi.data = 'href="newcss.css" type="text/css"';
-        expect(pi.data).toBe('href="newcss.css" type="text/css"');
-    });
+		pi.data = 'href="newcss.css" type="text/css"';
+		expect(pi.data).toBe('href="newcss.css" type="text/css"');
+	});
 });
