464751d88f6c20ff0383cfb8b9cd7c548a80532e
Ivan <admin@clab.hr>
2023-07-15 11:57:55 +0200

    format

--- test/error/reported.js
+++ test/error/reported.js
@@ -4,19 +4,16 @@ const path = require('path');
 
 /**
  * @typedef ErrorReport
- * @property {string} source the XML snippet
- * @property {'error'|'warning'|'fatalError'} level the name of the method triggered
- * @property {?function(msg:string):boolean} match to pick the relevant report when there are multiple
- * @property {?boolean} skippedInHtml Is the error reported when parsing HTML?
+ * @property {string} source The XML snippet
+ * @property {'error' | 'warning' | 'fatalError'} level The name of the method triggered
+ * @property {?function(msg:string):boolean} match To pick the relevant report when there are multiple
+ * @property {boolean | null} skippedInHtml Is the error reported when parsing HTML?
  */
-/**
- * A collection of XML samples and related information that cause the XMLReader
- * to call methods on `errorHandler`.
- */
+/** A collection of XML samples and related information that cause the XMLReader to call methods on `errorHandler`. */
 const REPORTED = {
 	/**
-	 * Entities need to be in the entityMap to be converted as part of parsing.
-	 * xmldom currently doesn't parse entities declared in DTD.
+	 * Entities need to be in the entityMap to be converted as part of parsing. xmldom currently doesn't parse entities declared in
+	 * DTD.
 	 *
 	 * @see https://www.w3.org/TR/2008/REC-xml-20081126/#wf-entdeclared
 	 * @see https://www.w3.org/TR/2006/REC-xml11-20060816/#wf-entdeclared
@@ -29,16 +26,15 @@ const REPORTED = {
 	/**
 	 * Well-formedness constraint: Unique Att Spec
 	 *
-	 * An attribute name must not appear more than once
-	 * in the same start-tag or empty-element tag.
+	 * An attribute name must not appear more than once in the same start-tag or empty-element tag.
 	 *
 	 * In the browser:
-	 * - as XML it is reported as `error on line 1 at column 17: Attribute a redefined`
-	 * - as HTML only the first definition is considered
 	 *
-	 * In xmldom the behavior is different for namespaces (picks first)
-	 * than for other attributes (picks last),
-	 * which can be a security issue.
+	 * - As XML it is reported as `error on line 1 at column 17: Attribute a redefined`
+	 * - As HTML only the first definition is considered
+	 *
+	 * In xmldom the behavior is different for namespaces (picks first) than for other attributes (picks last), which can be a
+	 * security issue.
 	 *
 	 * @see https://www.w3.org/TR/2008/REC-xml-20081126/#uniqattspec
 	 * @see https://www.w3.org/TR/2006/REC-xml11-20060816/#uniqattspec
@@ -49,9 +45,8 @@ const REPORTED = {
 		match: (msg) => /Attribute .* redefined/.test(msg),
 	},
 	/**
-	 * This sample doesn't follow the specified grammar.
-	 * In the browser it is reported as `error on line 1 at column 14: expected '>'`,
-	 * but still adds the root level element to the dom.
+	 * This sample doesn't follow the specified grammar. In the browser it is reported as `error on line 1 at column 14: expected
+	 * '>'`, but still adds the root level element to the dom.
 	 */
 	SYNTAX_EndTagNotComplete: {
 		source: '<xml></xml',
@@ -59,9 +54,8 @@ const REPORTED = {
 		match: (msg) => /end tag name/.test(msg) && /is not complete/.test(msg),
 	},
 	/**
-	 * This sample doesn't follow the specified grammar.
-	 * In the browser it is reported as `error on line 1 at column 21: expected '>'`,
-	 * but still adds the root level element and inner tag to the dom.
+	 * This sample doesn't follow the specified grammar. In the browser it is reported as `error on line 1 at column 21: expected
+	 * '>'`, but still adds the root level element and inner tag to the dom.
 	 */
 	SYNTAX_EndTagMaybeNotComplete: {
 		source: '<xml><inner></inner </xml>',
@@ -70,8 +64,8 @@ const REPORTED = {
 		match: (msg) => /end tag name/.test(msg) && /maybe not complete/.test(msg),
 	},
 	/**
-	 * This sample doesn't follow the specified grammar.
-	 * In the browser it is reported as `error on line 1 at column 6: Comment not terminated`.
+	 * This sample doesn't follow the specified grammar. In the browser it is reported as `error on line 1 at column 6: Comment not
+	 * terminated`.
 	 */
 	WF_UnclosedComment: {
 		source: '<xml></xml><!--',
@@ -79,13 +73,10 @@ const REPORTED = {
 		match: (msg) => /comment is not well-formed/.test(msg),
 	},
 	/**
-	 * Triggered by lib/sax.js:596, caught in 208
-	 * This sample doesn't follow the specified grammar.
-	 * In the browser:
-	 * - as XML it is reported as
-	 * `error on line 1 at column 2: StartTag: invalid element name`
-	 * - as HTML it is accepted as characters
+	 * Triggered by lib/sax.js:596, caught in 208 This sample doesn't follow the specified grammar. In the browser:
 	 *
+	 * - As XML it is reported as `error on line 1 at column 2: StartTag: invalid element name`
+	 * - As HTML it is accepted as characters
 	 */
 	SYNTAX_InvalidTagName: {
 		source: '<xml><123 /></xml>',
@@ -93,12 +84,10 @@ const REPORTED = {
 		match: (msg) => /invalid tagName/.test(msg),
 	},
 	/**
-	 * Triggered by lib/sax.js:602, caught in 208
-	 * This sample doesn't follow the specified grammar.
-	 * In the browser:
-	 * - as XML it is reported as
-	 * `error on line 1 at column 6: error parsing attribute name`
-	 * - as HTML it is accepted as attribute name
+	 * Triggered by lib/sax.js:602, caught in 208 This sample doesn't follow the specified grammar. In the browser:
+	 *
+	 * - As XML it is reported as `error on line 1 at column 6: error parsing attribute name`
+	 * - As HTML it is accepted as attribute name
 	 */
 	SYNTAX_InvalidAttributeName: {
 		source: '<xml><child 123=""/></xml>',
@@ -106,8 +95,8 @@ const REPORTED = {
 		match: (msg) => /invalid attribute/.test(msg),
 	},
 	/**
-	 * This sample doesn't follow the specified grammar.
-	 * In the browser it is reported as `error on line 1 at column 5: Couldn't find end of Start Tag xml`.
+	 * This sample doesn't follow the specified grammar. In the browser it is reported as `error on line 1 at column 5: Couldn't
+	 * find end of Start Tag xml`.
 	 */
 	SYNTAX_UnexpectedEndOfInput: {
 		source: '<xml',
@@ -115,11 +104,10 @@ const REPORTED = {
 		match: (msg) => /unexpected end of input/.test(msg),
 	},
 	/**
-	 * Triggered by lib/sax.js:392, caught in 208
-	 * This sample doesn't follow the specified grammar.
-	 * In the browser:
-	 * - in XML it is reported as `error on line 1 at column 8: error parsing attribute name`
-	 * - in HTML it produces `<xml><a <="" xml=""></a></xml>` (invalid XML?)
+	 * Triggered by lib/sax.js:392, caught in 208 This sample doesn't follow the specified grammar. In the browser:
+	 *
+	 * - In XML it is reported as `error on line 1 at column 8: error parsing attribute name`
+	 * - In HTML it produces `<xml><a <="" xml=""></a></xml>` (invalid XML?)
 	 */
 	SYNTAX_ElementClosingNotConnected: {
 		source: '<xml><a/ </xml>',
@@ -127,9 +115,8 @@ const REPORTED = {
 		match: (msg) => /must be connected/.test(msg),
 	},
 	/**
-	 * In the Browser (for XML) this is reported as
-	 * `error on line 1 at column 6: Extra content at the end of the document`
-	 * for HTML it's added to the DOM without anything being reported.
+	 * In the Browser (for XML) this is reported as `error on line 1 at column 6: Extra content at the end of the document` for HTML
+	 * it's added to the DOM without anything being reported.
 	 */
 	WF_UnclosedXmlAttribute: {
 		source: '<xml>',
@@ -139,10 +126,9 @@ const REPORTED = {
 	},
 	/**
 	 * In the browser:
-	 * - for XML it is reported as
-	 * `error on line 1 at column 10: Specification mandates value for attribute attr`
-	 * - for HTML is uses the attribute as one with no value and adds `"value"` to the attribute name
-	 *   and is not reporting any issue.
+	 *
+	 * - For XML it is reported as `error on line 1 at column 10: Specification mandates value for attribute attr`
+	 * - For HTML is uses the attribute as one with no value and adds `"value"` to the attribute name and is not reporting any issue.
 	 */
 	WF_AttributeValueMustAfterEqual: {
 		source: '<xml attr"value" />',
@@ -151,8 +137,9 @@ const REPORTED = {
 	},
 	/**
 	 * In the browser:
-	 * - for XML it is reported as `error on line 1 at column 11: AttValue: " or ' expected`
-	 * - for HTML is wraps `value"` with quotes and is not reporting any issue.
+	 *
+	 * - For XML it is reported as `error on line 1 at column 11: AttValue: " or ' expected`
+	 * - For HTML is wraps `value"` with quotes and is not reporting any issue.
 	 */
 	WF_AttributeMissingStartingQuote: {
 		source: '<xml attr=value" />',
@@ -160,12 +147,12 @@ const REPORTED = {
 		match: (msg) => /missed start quot/.test(msg),
 	},
 	/**
-	 * Triggered by lib/sax.js:264, caught in 208.
-	 * TODO: Comment indicates fatalError, change to use errorHandler.fatalError?
+	 * Triggered by lib/sax.js:264, caught in 208. TODO: Comment indicates fatalError, change to use errorHandler.fatalError?
 	 *
 	 * In the browser:
-	 * - for XML it is reported as `error on line 1 at column 20: AttValue: ' expected`
-	 * - for HTML nothing is added to the DOM.
+	 *
+	 * - For XML it is reported as `error on line 1 at column 20: AttValue: ' expected`
+	 * - For HTML nothing is added to the DOM.
 	 */
 	SYNTAX_AttributeMissingEndingQuote: {
 		source: '<xml><child attr="value /></xml>',
@@ -173,10 +160,10 @@ const REPORTED = {
 		match: (msg) => /attribute value no end .* match/.test(msg),
 	},
 	/**
-	 * Triggered by lib/sax.js:324
-	 * In the browser:
-	 * - for XML it is reported as `error on line 1 at column 11: AttValue: " or ' expected`
-	 * - for HTML is wraps `value/` with quotes and is not reporting any issue.
+	 * Triggered by lib/sax.js:324 In the browser:
+	 *
+	 * - For XML it is reported as `error on line 1 at column 11: AttValue: " or ' expected`
+	 * - For HTML is wraps `value/` with quotes and is not reporting any issue.
 	 */
 	WF_AttributeMissingQuote: {
 		source: '<xml attr=value/>',
@@ -184,15 +171,13 @@ const REPORTED = {
 		match: (msg) => / missed quot/.test(msg) && /!!/.test(msg) === false,
 	},
 	/**
-	 * Triggered by lib/sax.js:354
-	 * This is the only warning reported in this sample.
-	 * For some reason the "attribute" that is reported as missing quotes
-	 * has the name `&`.
-	 * This case is also present in 2 tests in test/html/normalize.test.js
+	 * Triggered by lib/sax.js:354 This is the only warning reported in this sample. For some reason the "attribute" that is
+	 * reported as missing quotes has the name `&`. This case is also present in 2 tests in test/html/normalize.test.js
 	 *
 	 * In the browser:
-	 * - for XML it is reported as `error on line 1 at column 8: AttValue: " or ' expected`
-	 * - for HTML is yields `<xml a="&amp;" b="&amp;"></xml>` and is not reporting any issue.
+	 *
+	 * - For XML it is reported as `error on line 1 at column 8: AttValue: " or ' expected`
+	 * - For HTML is yields `<xml a="&amp;" b="&amp;"></xml>` and is not reporting any issue.
 	 */
 	WF_AttributeMissingQuote2: {
 		source: `<xml a=& b="&"/>`,
@@ -201,10 +186,12 @@ const REPORTED = {
 	},
 	/**
 	 * In the browser:
-	 * - for XML it is reported as `error on line 1 at column 9: AttValue: " or ' expected`
-	 * - for HTML is yields `<doc a1></xml>` and is not reporting any issue.
+	 *
+	 * - For XML it is reported as `error on line 1 at column 9: AttValue: " or ' expected`
+	 * - For HTML is yields `<doc a1></xml>` and is not reporting any issue.
 	 *
 	 * But the XML specifications does not allow that:
+	 *
 	 * @see https://www.w3.org/TR/2008/REC-xml-20081126/#NT-Attribute
 	 * @see https://www.w3.org/TR/2006/REC-xml11-20060816/#NT-Attribute
 	 */
@@ -214,8 +201,9 @@ const REPORTED = {
 		match: (msg) => /attribute value missed!!/.test(msg),
 	},
 	/**
-	 * In the browser this is not an issue at all, but just add an attribute without a value.
-	 * But the XML specifications does not allow that:
+	 * In the browser this is not an issue at all, but just add an attribute without a value. But the XML specifications does not
+	 * allow that:
+	 *
 	 * @see https://www.w3.org/TR/2008/REC-xml-20081126/#NT-Attribute
 	 * @see https://www.w3.org/TR/2006/REC-xml11-20060816/#NT-Attribute
 	 */
@@ -226,10 +214,10 @@ const REPORTED = {
 		skippedInHtml: true,
 	},
 	/**
-	 * Triggered by lib/sax.js:376
-	 * This seems to only be reached when there are two subsequent attributes with a missing value
-	 * In the browser this is not an issue at all, but just add an attribute without a value.
-	 * But the XML specifications does not allow that:
+	 * Triggered by lib/sax.js:376 This seems to only be reached when there are two subsequent attributes with a missing value In
+	 * the browser this is not an issue at all, but just add an attribute without a value. But the XML specifications does not allow
+	 * that:
+	 *
 	 * @see https://www.w3.org/TR/2008/REC-xml-20081126/#NT-Attribute
 	 * @see https://www.w3.org/TR/2006/REC-xml11-20060816/#NT-Attribute
 	 */
@@ -246,21 +234,19 @@ const LINE_TO_ERROR_INDEX = {
 };
 
 /**
- * To avoid to have exact lines in snapshots, but still being able to verify,
- * that a certain error was reported in the expected order,
- * this method indexes all cases of
- * - thrown errors
- * - calls to one of the errorHandler methods
- * and adds them to the exported LINE_TO_ERROR_INDEX.
+ * To avoid to have exact lines in snapshots, but still being able to verify, that a certain error was reported in the expected
+ * order, this method indexes all cases of
  *
- * It also checks that every match configured in REPORTED only matches a single line,
- * and adds the related key to the index as `reportedAs`.
- * Any failing check will throw, so it prevents the tests from being executed.
+ * - Thrown errors
+ * - Calls to one of the errorHandler methods and adds them to the exported LINE_TO_ERROR_INDEX.
  *
- * The result is written to reported.json for easier human introspection.
- * The file is only written, not read by any code, the source code is the only source of truth.
+ * It also checks that every match configured in REPORTED only matches a single line, and adds the related key to the index as
+ * `reportedAs`. Any failing check will throw, so it prevents the tests from being executed.
  *
- * @param fileNameInKey the part of the path that is supposed to be part of the key
+ * The result is written to reported.json for easier human introspection. The file is only written, not read by any code, the
+ * source code is the only source of truth.
+ *
+ * @param fileNameInKey The part of the path that is supposed to be part of the key
  */
 function parseErrorLines(fileNameInKey) {
 	let errorIndex = 0;
--- test/get-test-parser.js
+++ test/get-test-parser.js
@@ -1,27 +1,21 @@
 'use strict';
 const { DOMParser } = require('../lib/dom-parser');
 
-/**
- * @typedef ErrorLevel {'warn' | 'error' | 'fatalError'}
- */
+/** @typedef ErrorLevel {'warn' | 'error' | 'fatalError'} */
 
 /**
- * Helper method for configuring an instance of DOMParser.
- * Calling it without any arguments allows to assert on `errors` after using the parser.
- * every field of the first argument is options and allows to specify test specific behavior.
- * - `errorHandler`: The `errorHandler` to pass to DOMParser constructor options,
- * 									default stores a list of all entries per `key` in `errors` and does not log or throw.
- * - `errors`: the object for the `errorHandler` to use,
- * 						is also returned with the same name for later assertions,
- * 						default is an empty object
+ * Helper method for configuring an instance of DOMParser. Calling it without any arguments allows to assert on `errors` after
+ * using the parser. every field of the first argument is options and allows to specify test specific behavior.
+ *
+ * - `errorHandler`: The `errorHandler` to pass to DOMParser constructor options, default stores a list of all entries per `key` in
+ *   `errors` and does not log or throw.
+ * - `errors`: the object for the `errorHandler` to use, is also returned with the same name for later assertions, default is an
+ *   empty object
  * - `locator`: Whether to record node locations in the XML string, default is true
  *
- * @param options {{
- * 					onError?: function (level:string, msg:string, context:DOMHandler),
- * 					errors?: [ErrorLevel, string, object][],
- * 					locator?: boolean
- *				}}
- * @returns {{parser: DOMParser, errors: [ErrorLevel, string, object][]}}
+ * @param options {{ onError?: function (level:string, msg:string, context:DOMHandler), errors?: [ErrorLevel, string, object][],
+ *   locator?: boolean }}
+ * @returns {{ parser: DOMParser; errors: [ErrorLevel, string, object][] }}
  */
 function getTestParser({ onError, errors = [], locator = true } = {}) {
 	onError =
