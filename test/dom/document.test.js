xmldom/xmldom

commit 5705f5df08ea4332f3f39a495b466b6e2c7f8f01

fix: restore performance for large amount of child nodes (#760)
as part of https://github.com/xmldom/xmldom/pull/550 ("drop redundant
and broken `Element.appendChild` implementation")
the `_onUpdateChild` was not passed the added child, so the `childNodes`
were "re-indexed" from the `firstChild` each time a node was appended.
This is why the performance degraded significantly, when there are a lot
of childNodes to be appended.

fixes https://github.com/xmldom/xmldom/issues/748

lib/dom.js
@@ -2054,7 +2054,7 @@ function _insertBefore(parent, node, child, _inDocumentAssertion) {
 	do {
 		newFirst.parentNode = parent;
 	} while (newFirst !== newLast && (newFirst = newFirst.nextSibling));
-	_onUpdateChild(parent.ownerDocument || parent, parent);
+	_onUpdateChild(parent.ownerDocument || parent, parent, node);
 	if (node.nodeType == DOCUMENT_FRAGMENT_NODE) {
 		node.firstChild = node.lastChild = null;
 	}

test/dom/node.test.js
@@ -2,8 +2,11 @@
 
 const { describe, test, expect } = require('@jest/globals');
 const { DOMImplementation, Node } = require('../../lib/dom');
+const { DOMParser } = require('../../lib/dom-parser');
 const { DOMExceptionName } = require('../../lib/errors');
 const { expectDOMException } = require('../errors/expectDOMException');
+const { MIME_TYPE } = require('../../lib/conventions');
+const { performance } = require('perf_hooks');
 
 describe('Node.prototype', () => {
 	describe('constructor', () => {
@@ -27,6 +30,17 @@ describe('Node.prototype', () => {
 					`Unexpected parent node type ${node.nodeType}`
 				);
 			});
+		});
+		const MANY = 10 * 1000;
+		const huge = `<xml>${[...Array(MANY).keys()].map((i) => `<node index="${i}"/>`).join('\n\t')}</xml>`;
+		test(`should be able to parse and append ${MANY / 1000}k nodes with a good performance`, () => {
+			const start = performance.now();
+			new DOMParser().parseFromString(huge, MIME_TYPE.XML_TEXT);
+			const duration = performance.now() - start;
+			// with the issue the test was introduced for,
+			// it took minutes for such an amount of nodes to be appended
+			// it usually takes < 1sec on my machine, but let's make sure the test is not flaky anywhere
+			expect(duration).toBeLessThanOrEqual(1500);
 		});
 	});
 	describe('isConnected', () => {

commit 686a301d0dae3b7e276cb047f13b62f56d15bf82

fix: re-index childNodes on insertBefore
to resolve a regression introduced in #760

fixes #763

lib/dom.js
@@ -1623,30 +1623,29 @@ function _onRemoveAttribute(doc, el, newAttr, remove) {
 }
 
 /**
- * Updates `el.childNodes`, adjusting the indexed items and its `length`.
- * If `newChild` is provided, it will be appended to the childNodes list.
- * Otherwise, it's assumed that an item has been removed,
- * and `el.firstNode` and its `.nextSibling` are used to iterate over the current list of child
- * nodes, effectively reindexing them.
+ * Updates `parent.childNodes`, adjusting the indexed items and its `length`.
+ * If `newChild` is provided and has no nextSibling, it will be appended.
+ * Otherwise, it's assumed that an item has been removed or inserted,
+ * and `parent.firstNode` and its `.nextSibling` to re-indexing all child nodes of `parent`.
  *
  * @param {Document} doc
  * The parent document of `el`.
- * @param {Node} el
+ * @param {Node} parent
  * The parent node whose childNodes list needs to be updated.
  * @param {Node} [newChild]
  * The new child node to be appended. If not provided, the function assumes a node has been
  * removed.
  * @private
  */
-function _onUpdateChild(doc, el, newChild) {
+function _onUpdateChild(doc, parent, newChild) {
 	if (doc && doc._inc) {
 		doc._inc++;
 		//update childNodes
-		var cs = el.childNodes;
-		if (newChild) {
+		var cs = parent.childNodes;
+		if (newChild && !newChild.nextSibling) {
 			cs[cs.length++] = newChild;
 		} else {
-			var child = el.firstChild;
+			var child = parent.firstChild;
 			var i = 0;
 			while (child) {
 				cs[i++] = child;

test/dom/document.test.js
@@ -416,6 +416,27 @@ describe('Document.prototype', () => {
 			expect(doc.childNodes).toHaveLength(0);
 			expect(root.parentNode).toBeNull();
 		});
+		test('should be insert doctype between processing instruction and element', () => {
+			const doc = new DOMImplementation().createDocument(null, '');
+			expect(doc.childNodes).toHaveLength(0);
+			expect(doc.documentElement).toBeNull();
+
+			const instruction = doc.createProcessingInstruction('target', 'data');
+			doc.appendChild(instruction);
+
+			const root = doc.createElement('root');
+			doc.appendChild(root);
+			expect(doc.childNodes).toHaveLength(2);
+			expect(doc.childNodes.item(0)).toBe(instruction);
+			expect(doc.childNodes.item(1)).toBe(root);
+
+			const doctype = doc.implementation.createDocumentType('qualifiedName', '', '');
+			doc.insertBefore(doctype, root);
+			expect(doc.childNodes).toHaveLength(3);
+			expect(doc.childNodes.item(0)).toBe(instruction);
+			expect(doc.childNodes.item(1)).toBe(doctype); // This assertion fails in 0.9.4, but passes in 0.9.3
+			expect(doc.childNodes.item(2)).toBe(root);
+		});
 	});
 	describe('replaceChild', () => {
 		test('should remove the only element and add the new one', () => {

test/dom/element.test.js
@@ -9,11 +9,6 @@ const { DOMException, DOMExceptionName } = require('../../lib/errors');
 const { expectDOMException } = require('../errors/expectDOMException');
 
 describe('documentElement', () => {
-	describe('constructor', () => {
-		test('should throw Illegal constructor TypeError when trying to access constructor directly', () => {
-			expect(() => new Element()).toThrow(TypeError);
-		});
-	});
 	test('can properly append exist child', () => {
 		const doc = new DOMParser().parseFromString(
 			'<xml xmlns="http://test.com" id="root">' +
@@ -157,6 +152,11 @@ describe('Element', () => {
 	const ATTR_LOWER_CASE = 'attr';
 	const VALUE = '2039e2dk';
 	describe('constructor', () => {
+		test('should throw Illegal constructor TypeError when trying to access constructor directly', () => {
+			expect(() => new Element()).toThrow(TypeError);
+		});
+	});
+	test('_nsMap has no prototype properties', () => {
 		const element = new DOMImplementation().createDocument(null, 'doc').documentElement;
 		expect(element._nsMap).not.toHaveProperty('prototype');
 		expect(element._nsMap).not.toHaveProperty('__proto__');