289d7c73aa7e9886681f437768c98262caccbe54
karfau <christian.bewernitz@bettermarks.com>
2026-03-29 19:52:32 +0200

    fix: split CDATASection at ']]>' in XMLSerializer to prevent injection
    Backport of the Phase 1b fix to release-0.8.x. Uses string literals
    ('<![CDATA[', ']]>') instead of the g.CDATA_START/END constants from main.
    Mutation-vector tests (appendData, replaceData, .data =, .textContent =)
    added to test/dom/serializer.test.js.
    
    Refs: GHSA-wh4c-j3r5-mjhp

--- index.d.ts
+++ index.d.ts
@@ -23,6 +23,16 @@ declare module "@xmldom/xmldom" {
   }
 
   interface XMLSerializer {
+      /**
+       * Returns the result of serializing `node` to XML.
+       *
+       * __This implementation differs from the specification:__
+       * - CDATASection nodes whose data contains `]]>` are serialized by splitting the section
+       *   at each `]]>` occurrence (following W3C DOM Level 3 Core `split-cdata-sections`
+       *   default behaviour). A configurable option is not yet implemented.
+       *
+       * @see https://html.spec.whatwg.org/#dom-xmlserializer-serializetostring
+       */
       serializeToString(node: Node): string;
   }
 
