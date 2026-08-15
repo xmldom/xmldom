xmldom/xmldom

commit 87aa97b4f50c7f545608effc7369c4c3049e9e07

fix!: report more non well-formed cases (#519)
`fatalError` will throw a `ParseError` and stop processing the document:
- consistently detect current tag name
- report all possible cases of mismatching or unclosed tags in XML
  - case mismatches in HTML documents are not reported
- report missing attribute value in XML
- report unescaped `<` in AttrValue as fatalError (only in XML
documents)
- report occurrences of Unicode replacement character `\ufffd`

`error` doesn't stop processing unless `onErrorStopParsing` or a similar
method is used:
- report non white-space characters outside of elements (at the same
level as the root element)

BREAKING-CHANGE: Reports more not well-formed documents as fatalError
and drop broken support for optional and unclosed tags in HTML.

Closes #45 
Closes #125
Closes #467

lib/sax.js
@@ -34,8 +34,20 @@ XMLReader.prototype = {
 	},
 };
 
+/**
+ * Detecting everything that might be a reference,
+ * including those without ending `;`, since those are allowed in HTML.
+ * The entityReplacer takes care of verifying and transforming each occurrence,
+ * and reports to the errorHandler on those that are not OK,
+ * depending on the context.
+ */
+var ENTITY_REG = /&#?\w+;?/g;
+
 function parse(source, defaultNSMapCopy, entityMap, domBuilder, errorHandler) {
 	var isHTML = isHTMLMimeType(domBuilder.mimeType);
+	if (source.indexOf(g.UNICODE_REPLACEMENT_CHARACTER) >= 0) {
+		return errorHandler.fatalError('Unicode replacement character detected, source encoding issues?');
+	}
 
 	function fixedFromCharCode(code) {
 		// String.prototype.fromCharCode does not supports
@@ -52,7 +64,17 @@ function parse(source, defaultNSMapCopy, entityMap, domBuilder, errorHandler) {
 	}
 
 	function entityReplacer(a) {
-		var k = a.slice(1, -1);
+		var complete = a[a.length - 1] === ';' ? a : a + ';';
+		if (!isHTML && complete !== a) {
+			errorHandler.error('EntityRef: expecting ;');
+			return a;
+		}
+		var match = g.Reference.exec(complete);
+		if (!match || match[0].length !== complete.length) {
+			errorHandler.error('entity not matching Reference production: ' + a);
+			return a;
+		}
+		var k = complete.slice(1, -1);
 		if (Object.hasOwnProperty.call(entityMap, k)) {
 			return entityMap[k];
 		} else if (k.charAt(0) === '#') {
@@ -66,7 +88,7 @@ function parse(source, defaultNSMapCopy, entityMap, domBuilder, errorHandler) {
 	function appendText(end) {
 		//has some bugs
 		if (end > start) {
-			var xt = source.substring(start, end).replace(/&#?\w+;/g, entityReplacer);
+			var xt = source.substring(start, end).replace(ENTITY_REG, entityReplacer);
 			locator && position(start);
 			domBuilder.characters(xt, 0, end - start);
 			start = end;
@@ -88,70 +110,83 @@ function parse(source, defaultNSMapCopy, entityMap, domBuilder, errorHandler) {
 	var locator = domBuilder.locator;
 
 	var parseStack = [{ currentNSMap: defaultNSMapCopy }];
-	var closeMap = {};
+	var unclosedTags = [];
 	var start = 0;
 	while (true) {
 		try {
 			var tagStart = source.indexOf('<', start);
 			if (tagStart < 0) {
-				if (!source.substr(start).match(/^\s*$/)) {
+				if (!isHTML && unclosedTags.length > 0) {
+					return errorHandler.fatalError('unclosed xml tag(s): ' + unclosedTags.join(', '));
+				}
+				if (!source.substring(start).match(/^\s*$/)) {
 					var doc = domBuilder.doc;
 					var text = doc.createTextNode(source.substr(start));
+					if (doc.documentElement) {
+						return errorHandler.error('Extra content at the end of the document');
+					}
 					doc.appendChild(text);
 					domBuilder.currentElement = text;
 				}
 				return;
 			}
 			if (tagStart > start) {
+				var fromSource = source.substring(start, tagStart);
+				if (!isHTML && unclosedTags.length === 0) {
+					fromSource = fromSource.replace(new RegExp(g.S_OPT.source, 'g'), '');
+					fromSource && errorHandler.error("Unexpected content outside root element: '" + fromSource + "'");
+				}
 				appendText(tagStart);
 			}
 			switch (source.charAt(tagStart + 1)) {
 				case '/':
-					var config = parseStack.pop();
-					var end = source.indexOf('>', tagStart + 3);
+					var end = source.indexOf('>', tagStart + 2);
 					var tagNameRaw = source.substring(tagStart + 2, end > 0 ? end : undefined);
-					var tagNameMatch = g.QName_group.exec(tagNameRaw);
-					// for the root level the config does not contain the tagName
-					var tagName =
-						tagNameMatch && tagNameMatch[1] ? tagNameMatch[1] : config.tagName || domBuilder.doc.documentElement.tagName;
-					if (end < 0) {
-						errorHandler.error('end tag name: ' + tagName + ' is not complete');
-						end = tagStart + 1 + tagName.length;
-					} else if (tagNameRaw.match(/</) && !isHTML) {
-						errorHandler.error('end tag name: ' + tagName + ' maybe not complete');
+					if (!tagNameRaw) {
+						return errorHandler.fatalError('end tag name missing');
 					}
+					var tagNameMatch = end > 0 && g.reg('^', g.QName_group, g.S_OPT, '$').exec(tagNameRaw);
+					if (!tagNameMatch) {
+						return errorHandler.fatalError('end tag name contains invalid characters: "' + tagNameRaw + '"');
+					}
+					if (!domBuilder.currentElement && !domBuilder.doc.documentElement) {
+						// not enough information to provide a helpful error message,
+						// but parsing will throw since there is no root element
+						return;
+					}
+					var currentTagName =
+						unclosedTags[unclosedTags.length - 1] ||
+						domBuilder.currentElement.tagName ||
+						domBuilder.doc.documentElement.tagName ||
+						'';
+					if (currentTagName !== tagNameMatch[1]) {
+						var tagNameLower = tagNameMatch[1].toLowerCase();
+						if (!isHTML || currentTagName.toLowerCase() !== tagNameLower) {
+							return errorHandler.fatalError('Opening and ending tag mismatch: "' + currentTagName + '" != "' + tagNameRaw + '"');
+						}
+					}
+					var config = parseStack.pop();
+					unclosedTags.pop();
 					var localNSMap = config.localNSMap;
-					var endMatch = config.tagName == tagName;
-					var endIgnoreCaseMach = endMatch || (config.tagName && config.tagName.toLowerCase() == tagName.toLowerCase());
-					if (endIgnoreCaseMach) {
-						domBuilder.endElement(config.uri, config.localName, tagName);
-						if (localNSMap) {
-							for (var prefix in localNSMap) {
-								if (Object.prototype.hasOwnProperty.call(localNSMap, prefix)) {
-									domBuilder.endPrefixMapping(prefix);
-								}
+					domBuilder.endElement(config.uri, config.localName, currentTagName);
+					if (localNSMap) {
+						for (var prefix in localNSMap) {
+							if (Object.prototype.hasOwnProperty.call(localNSMap, prefix)) {
+								domBuilder.endPrefixMapping(prefix);
 							}
 						}
-						if (!endMatch) {
-							// No known test case
-							return errorHandler.fatalError(
-								'end tag name: ' + tagName + ' is not match the current start tagName:' + config.tagName
-							);
-						}
-					} else {
-						parseStack.push(config);
 					}
 
 					end++;
 					break;
-				// end elment
+				// end element
 				case '?': // <?...?>
 					locator && position(tagStart);
 					end = parseProcessingInstruction(source, tagStart, domBuilder, errorHandler);
 					break;
 				case '!': // <!doctype,<![CDATA,<!--
 					locator && position(tagStart);
-					end = parseDoctypeCommentOrCData(source, tagStart, domBuilder, errorHandler);
+					end = parseDoctypeCommentOrCData(source, tagStart, domBuilder, errorHandler, isHTML);
 					break;
 				default:
 					locator && position(tagStart);
@@ -161,10 +196,11 @@ function parse(source, defaultNSMapCopy, entityMap, domBuilder, errorHandler) {
 					var end = parseElementStartPart(source, tagStart, el, currentNSMap, entityReplacer, errorHandler, isHTML);
 					var len = el.length;
 
-					if (!el.closed && fixSelfClosed(source, end, el.tagName, closeMap)) {
-						el.closed = true;
-						if (!isHTML) {
-							errorHandler.warning('unclosed xml attribute');
+					if (!el.closed) {
+						if (isHTML && conventions.isHTMLVoidElement(el.tagName)) {
+							el.closed = true;
+						} else {
+							unclosedTags.push(el.tagName);
 						}
 					}
 					if (locator && len) {
@@ -228,13 +264,16 @@ function parseElementStartPart(source, start, el, currentNSMap, entityReplacer, 
 		if (el.attributeNames.hasOwnProperty(qname)) {
 			return errorHandler.fatalError('Attribute ' + qname + ' redefined');
 		}
+		if (!isHTML && value.indexOf('<') >= 0) {
+			return errorHandler.fatalError("Unescaped '<' not allowed in attributes values");
+		}
 		el.addValue(
 			qname,
 			// @see https://www.w3.org/TR/xml/#AVNormalize
 			// since the xmldom sax parser does not "interpret" DTD the following is not implemented:
 			// - recursive replacement of (DTD) entity references
 			// - trimming and collapsing multiple spaces into a single one for attributes that are not of type CDATA
-			value.replace(/[\t\n\r]/g, ' ').replace(/&#?\w+;/g, entityReplacer),
+			value.replace(/[\t\n\r]/g, ' ').replace(ENTITY_REG, entityReplacer),
 			startIndex
 		);
 	}
@@ -346,7 +385,9 @@ function parseElementStartPart(source, start, el, currentNSMap, entityReplacer, 
 						}
 						break;
 					case S_EQ:
-						throw new Error('attribute value missed!!');
+						if (!isHTML) {
+							return errorHandler.fatalError('AttValue: \' or " expected');
+						}
 				}
 				return p;
 			/*xml space '\x20' | #x9 | #xD | #xA; */
@@ -506,26 +547,12 @@ function parseHtmlSpecialContent(source, elStartEnd, tagName, entityReplacer, do
 		var text = source.substring(elStartEnd + 1, elEndStart);
 
 		if (isEscapableRaw) {
-			text = text.replace(/&#?\w+;/g, entityReplacer);
+			text = text.replace(ENTITY_REG, entityReplacer);
 		}
 		domBuilder.characters(text, 0, text.length);
 		return elEndStart;
 	}
 	return elStartEnd + 1;
-}
-
-function fixSelfClosed(source, elStartEnd, tagName, closeMap) {
-	//if(tagName in closeMap){
-	var pos = closeMap[tagName];
-	if (pos == null) {
-		pos = source.lastIndexOf('</' + tagName + '>');
-		if (pos < elStartEnd) {
-			pos = source.lastIndexOf('</' + tagName);
-		}
-		closeMap[tagName] = pos;
-	}
-	return pos < elStartEnd;
-	//}
 }
 
 function _copy(source, target) {
@@ -716,11 +743,12 @@ function parseDoctypeInternalSubset(p, errorHandler) {
  * the start index of the '<!'
  * @param {DOMHandler} domBuilder
  * @param {DOMHandler} errorHandler
+ * @param {boolean} isHTML
  * @returns {number | never} The end index of the element.
  * @throws {ParseError}
  * In case the element is not well-formed.
  */
-function parseDoctypeCommentOrCData(source, start, domBuilder, errorHandler) {
+function parseDoctypeCommentOrCData(source, start, domBuilder, errorHandler, isHTML) {
 	var p = parseUtils(source, start);
 
 	switch (p.char(2)) {
@@ -737,6 +765,9 @@ function parseDoctypeCommentOrCData(source, start, domBuilder, errorHandler) {
 			// should be CDATA
 			var cdata = p.getMatch(g.CDSect);
 			if (cdata) {
+				if (!isHTML && !domBuilder.currentElement) {
+					return errorHandler.fatalError('CDATA outside of element');
+				}
 				domBuilder.startCDATA();
 				domBuilder.characters(cdata, g.CDATA_START.length, cdata.length - g.CDATA_START.length - g.CDATA_END.length);
 				domBuilder.endCDATA();

lib/dom-parser.js
@@ -200,7 +200,7 @@ function DOMParser(options) {
  * @param {string} [mimeType='application/xml']
  * the mimeType or contentType of the document to be created determines the `type` of document
  * created (XML or HTML)
- * @returns The `Document` node.
+ * @returns {Document} The `Document` node.
  * @throws {ParseError}
  * for any `fatalError` or anything that is thrown by `onError`
  * @throws {TypeError}

lib/grammar.js
@@ -113,6 +113,16 @@ function regg(args) {
 // https://www.w3.org/TR/xml11/#NT-document
 // `[1] document ::= ( prolog element Misc* ) - ( Char* RestrictedChar Char* )`
 
+/**
+ * A character usually appearing in wrongly converted strings.
+ *
+ * @type {string}
+ * @see https://en.wikipedia.org/wiki/Specials_(Unicode_block)#Replacement_character
+ * @see https://nodejs.dev/en/api/v18/buffer/#buffers-and-character-encodings
+ * @see https://www.unicode.org/faq/utf_bom.html#BOM
+ * @readonly
+ */
+var UNICODE_REPLACEMENT_CHARACTER = '\uFFFD';
 // https://www.w3.org/TR/xml/#NT-Char
 // any Unicode character, excluding the surrogate blocks, FFFE, and FFFF.
 // `[2] Char ::= #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]`
@@ -500,6 +510,7 @@ exports.ExternalID = ExternalID;
 exports.ExternalID_match = ExternalID_match;
 exports.Name = Name;
 exports.NotationDecl = NotationDecl;
+exports.Reference = Reference;
 exports.PEReference = PEReference;
 exports.PI = PI;
 exports.PUBLIC = PUBLIC;
@@ -512,5 +523,6 @@ exports.SChar_s = SChar_s;
 exports.S_OPT = S_OPT;
 exports.SYSTEM = SYSTEM;
 exports.SystemLiteral = SystemLiteral;
+exports.UNICODE_REPLACEMENT_CHARACTER = UNICODE_REPLACEMENT_CHARACTER;
 exports.UNICODE_SUPPORT = UNICODE_SUPPORT;
 exports.XMLDecl = XMLDecl;

test/dom-parser.test.js
@@ -137,20 +137,19 @@ describe('DOMParser', () => {
 				const onError = jest.fn();
 				const parser = new DOMParser({ onError });
 
-				parser.parseFromString('<xml>', MIME_TYPE.XML_TEXT);
+				parser.parseFromString('<xml attr />', MIME_TYPE.XML_TEXT);
 
 				expect(onError).toHaveBeenCalledTimes(1);
-				expect(onError).toHaveBeenCalledWith('warning', expect.stringContaining('unclosed'), expect.any(__DOMHandler));
+				expect(onError).toHaveBeenCalledWith('warning', expect.stringContaining('attribute'), expect.any(__DOMHandler));
 			});
 			test('should be passed to DOMHandler and called for level error', () => {
 				const onError = jest.fn();
 				const parser = new DOMParser({ onError });
 
-				parser.parseFromString('<xml', MIME_TYPE.XML_TEXT);
+				parser.parseFromString(`<xml>&e;</xml>`, MIME_TYPE.XML_TEXT);
 
-				expect(onError).toHaveBeenCalledWith('error', expect.stringContaining('end'), expect.any(__DOMHandler));
-				expect(onError).toHaveBeenCalledWith('warning', expect.stringContaining('unclosed'), expect.any(__DOMHandler));
-				expect(onError).toHaveBeenCalledTimes(2);
+				expect(onError).toHaveBeenCalledWith('error', expect.stringContaining('entity'), expect.any(__DOMHandler));
+				expect(onError).toHaveBeenCalledTimes(1);
 			});
 			test('should be passed to DOMHandler and called for level fatalError', () => {
 				const onError = jest.fn();
@@ -162,24 +161,38 @@ describe('DOMParser', () => {
 				expect(onError).toHaveBeenCalledWith('fatalError', expect.stringContaining('root'), expect.any(__DOMHandler));
 			});
 			test('should throw for level error when using onErrorStopParsing', () => {
-				const parser = new DOMParser({ onError: onErrorStopParsing });
+				const onError = jest.fn(onErrorStopParsing);
+				const parser = new DOMParser({ onError });
 
 				// warning
-				expect(() => parser.parseFromString('<xml>', MIME_TYPE.XML_TEXT)).not.toThrow();
+				expect(() => parser.parseFromString('<xml attr />', MIME_TYPE.XML_TEXT)).not.toThrow(ParseError);
+				expect(onError).toBeCalledTimes(1);
+				expect(onError).toHaveBeenCalledWith('warning', expect.anything(), expect.anything());
 				// error
-				expect(() => parser.parseFromString('<xml', MIME_TYPE.XML_TEXT)).toThrow(ParseError);
+				expect(() => parser.parseFromString('<xml>&e;</xml>', MIME_TYPE.XML_TEXT)).toThrow(ParseError);
+				expect(onError).toBeCalledTimes(2);
+				expect(onError).toHaveBeenCalledWith('error', expect.anything(), expect.anything());
 				// fatalError
 				expect(() => parser.parseFromString('', MIME_TYPE.XML_TEXT)).toThrow(ParseError);
+				expect(onError).toBeCalledTimes(3);
+				expect(onError).toHaveBeenCalledWith('fatalError', expect.anything(), expect.anything());
 			});
 			test('should throw for level error when using onWarningStopParsing', () => {
-				const parser = new DOMParser({ onError: onWarningStopParsing });
+				const onError = jest.fn(onWarningStopParsing);
+				const parser = new DOMParser({ onError });
 
 				// warning
-				expect(() => parser.parseFromString('<xml>', MIME_TYPE.XML_TEXT)).toThrow(ParseError);
+				expect(() => parser.parseFromString('<xml attr />', MIME_TYPE.XML_TEXT)).toThrow(ParseError);
+				expect(onError).toBeCalledTimes(1);
+				expect(onError).toHaveBeenCalledWith('warning', expect.anything(), expect.anything());
 				// error
-				expect(() => parser.parseFromString('<xml', MIME_TYPE.XML_TEXT)).toThrow(ParseError);
+				expect(() => parser.parseFromString('<xml>&e;</xml>', MIME_TYPE.XML_TEXT)).toThrow(ParseError);
+				expect(onError).toBeCalledTimes(2);
+				expect(onError).toHaveBeenCalledWith('error', expect.anything(), expect.anything());
 				// fatalError
 				expect(() => parser.parseFromString('', MIME_TYPE.XML_TEXT)).toThrow(ParseError);
+				expect(onError).toBeCalledTimes(3);
+				expect(onError).toHaveBeenCalledWith('fatalError', expect.anything(), expect.anything());
 			});
 			test('should throw when errorHandler is not a function', () => {
 				expect(() => new DOMParser({ errorHandler: {} })).toThrow(TypeError);

test/dom/serializer.test.js
@@ -211,7 +211,7 @@ describe('XML Serializer', () => {
 		});
 
 		test('should escape special characters in namespace attributes', () => {
-			const input = `<xml xmlns='<&"' xmlns:attr='"&<'><test attr:test=""/></xml>`;
+			const input = `<xml xmlns='&lt;&"' xmlns:attr='"&&lt;'><test attr:test=""/></xml>`;
 			const doc = new DOMParser().parseFromString(input, MIME_TYPE.XML_TEXT);
 
 			// in this case the explicit attribute nodes are serialized

commit 7cfb9ea1fee0bf7fcca7340034f3c12c72744a82

docs(sax): explain the recoverable error for content outside the root
Add an inline comment at the "Extra content at the end of the document"
report: `return errorHandler.error` is an uncommon pattern (normally only
`fatalError`s stop parsing this way), used intentionally here to stop
parsing and return the Document after reporting the trailing content that
will not become part of it.

The recoverable-error categorization it documents was introduced in #519.

lib/sax.js
@@ -126,6 +126,10 @@ function parse(source, defaultNSMapCopy, entityMap, domBuilder, errorHandler) {
 					var doc = domBuilder.doc;
 					var text = doc.createTextNode(source.substring(start));
 					if (doc.documentElement) {
+						// `return errorHandler.error` is not a common pattern,
+						// it is usually only used with `.fatalError`s.
+						// In this case it is intentional, because it allows to stop parsing
+						// and returning doc after reporting the extra content that will not be part of the document.
 						return errorHandler.error('Extra content at the end of the document');
 					}
 					doc.appendChild(text);